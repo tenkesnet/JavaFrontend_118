db242d1d3aa8c63f8701c023eb2ea12d750f1311
Bárdó <rbardo@gmail.com>
2023-03-23 17:14:50 +0100

    2023.03.23 kezdés

---
+++ 4/ismetles.js
@@ -0,0 +1,28 @@
+let tomb = ["Alma", 'Körte', "Szilva"]
+let b = ["teve", 'kutya']
+let c = new Array("piros", "zold")
+
+for (let i = 0; i < tomb.length; i++) {
+    console.log(`tomb[${i}] = ${tomb[i]}`)
+    //console.log(a[i])
+}
+
+
+//Tömb elemeinek kiieatása
+for (let item of tomb) {
+    console.log(item)
+}
+
+
+for (let i = 3; i <= 1000; i++) {
+    let prim = true;
+    for (let j = 2; j < i - 1; j++) {
+        if (i % j === 0) {
+            prim = false
+        }
+    }
+    if (prim) {
+        console.log(`A ${i} az primszám.`)
+    }
+
+}
