9818c8e84ebb9530430c600fc18e4b419f417d38
Bárdó <rbardo@gmail.com>
2023-03-28 19:49:46 +0200

    OOP előtti állapot

--- 5/objektumok.js
+++ 5/objektumok.js
@@ -1,9 +1,65 @@
 let tomb = [1, 2, 3, 4]
-let a = {
-    nev: "Róbert",
-    foglalkozas: "programozó",
-    kor: 48
+
+// let dolgozo1 = {
+//     nev: "Róbert",
+//     foglalkozas: "programozó",
+//     kor: 48,
+//     szulEv: 0,
+// }
+
+//dolgozo1.foglalkozas = "fejleszto"
+
+let Szemely = function (nev, foglalkozas, kor) {
+    this.nev = nev
+    this.foglalkozas = foglalkozas
+    this.kor = kor
 }
+
+class Dolgozo {
+
+    szulEv = 0
+    constructor(nev, foglalkozas, kor) {
+        this.nev = nev
+        this.foglalkozas = foglalkozas
+        this.kor = kor
+    }
+    getFoglalkozas() {
+        return this.foglalkozas
+    }
+    getBirthDate() {
+        this.szulEv = 2023 - this.kor
+    }
+
+    toString() {
+        return `[ ${this.nev}, ${this.foglalkozas}, ${this.kor} ]`
+    }
+}
+
+let attila = new Dolgozo()
+
+let dolgozo1 = new Dolgozo("Róbert", "fejleszto", 48)
+//let dolgozo2 = new Dolgozo("Anna", "fejleszto", 21)
+//Dolgozo.prototype.szulEv = 0
+let dolgozo3 = new Dolgozo("Gábor", "eladó", 30)
+
+
+
+
+let dolgozo2 = {
+    nev: "Anna",
+    foglalkozas: "programozó",
+    kor: 22,
+    szulEv: 0,
+    getBirthDate: function () {
+        this.szulEv = 2023 - this.kor
+    },
+    toString: function () {
+        return `[ ${this.nev}, ${this.foglalkozas}, ${this.kor} ]`
+    }
+}
+
+let dolgozo4 = Object.create(dolgozo2)
+
 let b = 10
 let c = b
 b = 100
@@ -27,7 +83,11 @@ function valtoztat(x, y) {
     y = 100
 }
 
-valtoztat(a, b)
-
-console.log(b)
-console.log(a)
+//valtoztat(dolgozo1, b)
+dolgozo1.getBirthDate()
+//dolgozo3.getBirthDate()
+dolgozo4.getBirthDate()
+console.log(`Dolgozo1: ${dolgozo1}`)
+console.log(`Dolgozo3: ${dolgozo3}`)
+console.log(`Dolgozo4: ${dolgozo4}`)
+//console.log(dolgozo2)
