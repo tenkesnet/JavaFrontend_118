65c168ffa335cfa372ebcc5d8a56e6a79293f623
Bárdó <rbardo@gmail.com>
2023-04-04 19:02:22 +0200

    Bútorlap osztály elkészítése

--- 7/oop_pelda3.js
+++ 7/oop_pelda3.js
@@ -1,12 +1,36 @@
 class Butorlap {
 
-    constructor(tipus, szellessg, hosszusag) {
-
+    /**
+     * egy adott typus m2 egységára
+     */
+    artabla = {
+        butorlap: 5000,
+        hatlap: 600,
+        ajto: 7000
+    }
+    /**
+     * 
+     * @param {String} tipus -- Értéke lehet: "butorlap","hatlap"
+     * @param {number} szellesseg - Centiméterben meganadó szám
+     * @param {number} hosszusag - Centiméterben meganadó szám
+     */
+    constructor(tipus, szellesseg, hosszusag) {
+        this.tipus = tipus
+        this.szellesseg = szellesseg
+        this.hosszusag = hosszusag
+        if (!(this.tipus in this.artabla)) {
+            this.tipus = "butorlap"
+        }
     }
 
-    arSzamol() {
-
+    /**
+     * 
+     * @returns Number - a bútorlap ára
+     */
+    getAr() {
+        return this.artabla[this.tipus] * (this.szellesseg / 100) * (this.hosszusag / 100)
     }
+
 }
 
 class Butor {
@@ -32,3 +56,5 @@ class Butor {
 
     }
 }
+
+let b1 = new Butorlap("ajto", 300, 400)
