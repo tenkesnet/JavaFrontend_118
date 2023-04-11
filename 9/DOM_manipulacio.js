c29562cb76ea5212da9bfdd71b02e9e938c9f4f1
Bárdó <rbardo@gmail.com>
2023-04-11 19:45:12 +0200

    Mouse and input eseméynyek és a preventDefault() működése

--- 9/DOM_manipulacio.js
+++ 9/DOM_manipulacio.js
@@ -5,16 +5,46 @@ const body = document.getElementsByTagName("body")
 
 const labels = document.getElementsByTagName("label")
 const formGroups = document.getElementsByClassName("form-group")
+const submitButton = document.querySelector("input[type='submit']")
 
-console.log(container)
-console.log(emailInput)
-console.log(labels)
-console.log(formGroups[0])
-console.log(body)
+
+emailInput.focus()
+console.log(submitButton)
+
+submitButton.setAttribute("data", "name:Ede")
+
+submitButton.addEventListener("mouseenter", enterMouse)
+submitButton.addEventListener("mouseleave", (event) => {
+    event.target.classList.add("btn-success")
+    event.target.classList.remove("btn-danger")
+})
+submitButton.addEventListener("click", (event) => {
+    event.preventDefault()
+
+})
+
+emailInput.addEventListener("keypress", (evt) => {
+    if (evt.key == "Enter") {
+        document.querySelector("#password").focus()
+        evt.target.style.width = "100px"
+    }
+})
+
+
+function enterMouse(event) {
+    event.target.classList.add("btn-danger")
+    event.target.classList.remove("btn-success")
+    //console.log(event)
+}
+// console.log(container)
+// console.log(emailInput)
+// console.log(labels)
+// console.log(formGroups[0])
+// console.log(body)
 
 container.style.background = "lightblue"
 //container.className = "container bg-primary"
-container.classList.add("bg-danger")
-container.classList.remove("container")
+//container.classList.add("bg-danger")
+//container.classList.remove("container")
 
-labels[0].innerText = "<h1>Email cím</h1>"
+//labels[0].innerText = "<h1>Email cím</h1>"
