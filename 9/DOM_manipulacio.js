e1157d595bb31f583eae1c661e0c0900750b0331
Bárdó <rbardo@gmail.com>
2023-04-12 10:14:57 +0200

    setTimeout használata

--- 9/DOM_manipulacio.js
+++ 9/DOM_manipulacio.js
@@ -25,10 +25,33 @@ submitButton.addEventListener("click", (event) => {
 
 emailInput.addEventListener("keypress", (evt) => {
     if (evt.key == "Enter") {
-        document.querySelector("#password").focus()
-        evt.target.style.width = "100px"
+        if (evt.target.value.includes("@")) {
+            document.querySelector("#password").focus()
+        } else {
+            const errDiv = document.querySelector("#error")
+            errDiv.innerText = "Hibás emailcím"
+            setTimeout(function () {
+                errDiv.innerText = ""
+            }, 5000)
+        }
+
+        //evt.target.style.width = "100px"
     }
 })
+
+
+
+document.querySelector("#password").addEventListener("keypress", (evt) => {
+    if (evt.key == "Enter") {
+        const newEl = document.createElement("h3")
+        newEl.innerText = emailInput.value
+        const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+        newEl.style.background = '#' + randomColor
+        document.querySelector("#userlist").appendChild(newEl)
+    }
+})
+
+
 
 
 function enterMouse(event) {
