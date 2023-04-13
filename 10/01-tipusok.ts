b1728ba685118cd0990c89728422a86af70b5c36
Bárdó <rbardo@gmail.com>
2023-04-13 18:27:18 +0200

    10. óra típusok

---
+++ 10/01-tipusok.ts
@@ -0,0 +1,11 @@
+{
+    let nev: string = "Robi"
+    let age: number
+    let igazhamis: boolean = true
+
+    let nehasznald: any
+
+    nehasznald.sizetes()
+    console.log(`Hello ${nev}`)
+}
+
---
+++ 10/main.js
@@ -0,0 +1,8 @@
+{
+    let nev = "Robi";
+    let age;
+    let igazhamis = true;
+    let nehasznald;
+    nehasznald.sizetes();
+    console.log(`Hello ${nev}`);
+}
