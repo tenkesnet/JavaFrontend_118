ca885adbddc6b1778eab480e9574e915fcdb25f7
tenkesnet <rbardo@gmail.com>
2023-05-23 18:45:36 +0200

    20 - refactor adduser

--- 20/ex20/src/app/components/User/AddUser.tsx
+++ 20/ex20/src/app/components/User/AddUser.tsx
@@ -1,34 +1,25 @@
-import React, { useState, FormEvent, ChangeEvent } from 'react';
+import React, { useState, FormEvent, ChangeEvent, useRef } from 'react';
 
 import Card from '../UI/Card';
 import Button from '../UI/Button';
 import classes from './AddUser.module.css';
 
-
 const AddUser = () => {
-  const [enteredUsername, setEnteredUsername] = useState('');
-  const [enteredAge, setEnteredAge] = useState('');
+  const userNameRef = useRef<HTMLInputElement>(null);
+  const ageRef = useRef<HTMLInputElement>(null);
 
-  const addUserHandler = (event:FormEvent<HTMLFormElement>) => {
+  const addUserHandler = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    console.log(enteredUsername, enteredAge);
-  };
-
-  const usernameChangeHandler = (event:ChangeEvent<HTMLInputElement>) => {
-    setEnteredUsername(event.target.value);
-  };
-
-  const ageChangeHandler = (event:ChangeEvent<HTMLInputElement>) => {
-    setEnteredAge(event.target.value);
+    console.log(userNameRef.current?.value, ageRef.current?.value);
   };
 
   return (
     <Card className={classes.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
-        <input id="username" type="text" onChange={usernameChangeHandler} />
+        <input id="username" type="text" ref={userNameRef} />
         <label htmlFor="age">Age (Years)</label>
-        <input id="age" type="number" onChange={ageChangeHandler} />
+        <input id="age" type="number" ref={ageRef} />
         <Button type="submit">Add User</Button>
       </form>
     </Card>
