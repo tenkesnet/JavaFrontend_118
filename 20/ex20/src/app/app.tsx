44e92161cd8fb17bb557451fefe856851c947067
tenkesnet <rbardo@gmail.com>
2023-05-23 18:08:41 +0200

    20. refactor előtt

---
+++ 20/ex20/src/app/app.tsx
@@ -0,0 +1,15 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from './app.module.css';
+
+
+
+export function App() {
+  return (
+    <>
+      <h2>Hello</h2>
+      <h2>World!</h2>
+   </>
+  );
+}
+
+export default App;
