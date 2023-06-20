975ca06087b65707a156c05d6e072751c4d853ee
tenkesnet <rbardo@gmail.com>
2023-06-20 19:54:47 +0200

    12-Filesystem

---
+++ backend/filesystem.js
@@ -0,0 +1,44 @@
+var fs = require("fs");
+
+
+fs.readFile('pelda2.txt', function(err, fd) {
+ if (err) {
+ return console.error(err);
+ }
+    for(i=0;i<1;i++){
+        Math.sqrt(i);
+    }
+ console.log("File opened2 successfully!");
+});
+
+// Asynchronous read
+fs.readFile('pelda.txt', function (err, data) {
+ if (err) {
+ return console.error(err);
+ }
+ console.log("Asynchronous read: " + data.toString());
+});
+// Synchronous read
+var data = fs.readFileSync('pelda.txt');
+console.log("Synchronous read: " + data.toString());
+
+
+
+console.log("Going to get file info!");
+fs.stat('pelda.txt', function (err, stats) {
+ if (err) {
+ return console.error(err);
+ }
+ console.log(stats);
+ console.log("Got file info successfully!");
+
+ // Check file type
+ console.log("isFile ? " + stats.isFile());
+ console.log("isDirectory ? " + stats.isDirectory());
+});
+
+// Asynchronous - Opening File
+
+
+
+console.log("Program Ended");
--- backend/stream.js
+++ backend/stream.js
@@ -1,17 +1,23 @@
 var fs = require("fs");
 var data = '';
+let i = 0;
 // Create a readable stream
-var readerStream = fs.createReadStream('input.txt');
+var readerStream = fs.createReadStream('/home/bardo/siklos.sql');
 // Set the encoding to be utf8. 
 readerStream.setEncoding('UTF8');
 // Handle stream events --> data, end, and error
 readerStream.on('data', function(chunk) {
- data += chunk;
+    data += chunk;
 });
+readerStream.on('data', function(chunk) {
+    console.log("Chunk(" + i + "):");
+    i++;
+});
+
 readerStream.on('end',function(){
- console.log(data);
+ //console.log(data);
 });
-readerStream.on('error', function(err){
- console.log(err.stack);
+readerStream.on('error', function (err) {
+    console.log('Hiba: ',err.stack);
 });
 console.log("Program Ended");
