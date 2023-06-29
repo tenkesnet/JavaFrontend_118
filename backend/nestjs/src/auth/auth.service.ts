964ecc4b36fcb62518059704cc69cc86bcbd1b96
tenkesnet <rbardo@gmail.com>
2023-06-29 17:23:51 +0200

    Basic auth

--- backend/nestjs/src/auth/auth.service.ts
+++ backend/nestjs/src/auth/auth.service.ts
@@ -1,12 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Req } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { Request } from 'express';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly prisma: PrismaService) {}
+  constructor(private readonly prisma: PrismaService) { }
 
-  signup() {
-    return 'signup';
+  signup(@Req() req: Request) {
+    console
   }
 
   signin() {
--- backend/nestjs/src/main.ts
+++ backend/nestjs/src/main.ts
@@ -1,9 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  console.log('Server is running on port 3000.');
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // remove unknown properties from DTOs
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
