c78e88f45fc03a7950e6f2f0fc61b0d900dc077d
tenkesnet <rbardo@gmail.com>
2023-06-29 17:24:12 +0200

    Basic auth

--- backend/nestjs/src/auth/auth.service.ts
+++ backend/nestjs/src/auth/auth.service.ts
@@ -1,13 +1,39 @@
-import { Injectable, Req } from '@nestjs/common';
+import { ForbiddenException, Injectable, Req } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
-import { Request } from 'express';
+import * as argon from 'argon2';
+import { AuthDto } from './dto';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 
 @Injectable()
 export class AuthService {
   constructor(private readonly prisma: PrismaService) { }
 
-  signup(@Req() req: Request) {
-    console
+  async signup(dto: AuthDto) {
+    // generate the password hash
+    const hash = await argon.hash(dto.password);
+    // save the new user in the db
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+      });
+
+      return user;
+    } catch (error) {
+      if (
+        error instanceof
+        PrismaClientKnownRequestError
+      ) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException(
+            'Credentials taken',
+          );
+        }
+      }
+      throw error;
+    }
   }
 
   signin() {
