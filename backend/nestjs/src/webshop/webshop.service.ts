1f1f1d097fb2786c72219a6712cf2b2e25c9a4d1
tenkesnet <rbardo@gmail.com>
2023-07-05 04:48:34 +0200

    Webshop init

--- backend/nestjs/src/webshop/webshop.service.ts
+++ backend/nestjs/src/webshop/webshop.service.ts
@@ -1,4 +1,54 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class WebshopService {}
+export class WebshopService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  findAllCategorys() {
+    return this.prisma.category.findMany({
+      select: {
+        title: true,
+        products: {
+          select: {
+            id: true,
+            name: true,
+            price: true,
+            imageUrl: true,
+            updatedAt: false,
+            createdAt: false,
+          },
+        },
+      },
+    });
+  }
+
+  findAllCategorysCount() {
+    return this.prisma.category.findMany({
+      select: {
+        title: true,
+        _count: {
+          select: {
+            products: true,
+          },
+        },
+      },
+    });
+  }
+
+  findAllProducts() {
+    return this.prisma.product.findMany({
+      select: {
+        id: true,
+        name: true,
+        price: true,
+        imageUrl: true,
+        category: {
+          select: {
+            title: true,
+          },
+        },
+      },
+    });
+  }
+}
